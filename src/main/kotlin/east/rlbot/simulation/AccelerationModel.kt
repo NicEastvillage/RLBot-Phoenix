08f05f6909c80382acbba31f8f93673efa405af3
NicEastvillage <nicoesterby@gmail.com>
2021-09-06 21:18:15 +0200

    Added acceleration data for turning.

--- src/main/kotlin/east/rlbot/simulation/AccelerationModel.kt
+++ src/main/kotlin/east/rlbot/simulation/AccelerationModel.kt
@@ -3,6 +3,6 @@ package east.rlbot.simulation
 import java.io.File
 
 object AccelerationModel {
-    val throttle = AccelerationLUT(File(javaClass.getResource("/AccelerationData/throttle.csv").toURI()))
-    val boost = AccelerationLUT(File(javaClass.getResource("/AccelerationData/boost.csv").toURI()))
+    val throttle = AccelerationLUT(File(javaClass.getResource("/AccelerationData/StraightThrottle.csv").toURI()))
+    val boost = AccelerationLUT(File(javaClass.getResource("/AccelerationData/StraightBoost.csv").toURI()))
 }
